75e134f1916f6c2d4dbb8429fc66242fa0c5a49f
DNA Sistemas <52746721+dna-programacao@users.noreply.github.com>
2019-07-10 10:23:23 -0300

    commit
    commit inicial

---
+++ assets/js/home.js
@@ -0,0 +1,9 @@
+document.getElementById('btn-tv').onclick = function(){
+
+    document.getElementById("frame").src = 'https://get.teamviewer.com/dnasistemas';
+}
+
+document.getElementById('btn-ad').onclick = function(){
+    
+    document.getElementById("frame").src = 'https://download.anydesk.com/AnyDesk.exe?_ga=2.116056838.1229121362.1562713050-1525571773.1562713050';
+}
